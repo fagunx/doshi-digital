b897d74923df54ae2dd5676013440b4acde2dcab
fagunx <fagundoshi.2210@gmail.com>
2025-07-27 21:10:04 +0530

    host error fix trial - 1

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => {
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter basename="/doshi-digital/">
+            <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Index />} />
                 {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
